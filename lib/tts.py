141bfd3e4a3fd48cb8e6eca044e6d8e65a5923be
xuhuanju <xuhuanju@baidu.com>
2024-10-29 11:59:20 +0800

    fix children_books

--- lib/tts.py
+++ lib/tts.py
@@ -25,7 +25,11 @@ class TtsClient:
 
     def generate_tts(self, text):
         # 要检查的文件夹路径
-        directory = "../data/"
+        # 获取当前脚本的文件路径
+        current_file_path = __file__
+        # 获取目录
+        current_directory = os.path.dirname(os.path.abspath(current_file_path))
+        directory = f"{current_directory}/../data"
 
         # 检查文件夹是否存在
         if not os.path.exists(directory):
@@ -36,7 +40,7 @@ class TtsClient:
                                         sample_rate=48000)
         if result.get_audio_data() is not None:
             file_name = str(uuid.uuid4())
-            with open(f'../data/{file_name}.wav', 'wb') as f:
+            with open(f'{directory}/{file_name}.wav', 'wb') as f:
                 f.write(result.get_audio_data())
-            return f'../data/{file_name}.wav'
+            return f'{directory}/{file_name}.wav'
 
