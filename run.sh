dffc28ca960f9af1112f3022f0034e60173d4686
xuhuanju <xuhuanju@baidu.com>
2024-10-29 11:37:40 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# 获取当前脚本的绝对路径
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+
+# 要检查的文件路径
+FILE="$SCRIPT_DIR/lib/.env"
+
+# 检查文件是否存在
+if [ ! -e "$FILE" ]; then
+    echo "env 文件不存在: $FILE"
+    exit
+fi
 pip install -r requirements.txt
 python bot/server.py
