38207252214acc88e16eeb26547afac6de0a30bd
xuhuanju <xuhuanju@baidu.com>
2025-02-06 17:39:49 +0800

    optimize R1 think output

--- lib/qianfan.py
+++ lib/qianfan.py
@@ -38,8 +38,8 @@ class QianfanClient:
                 if choice.delta.content:
                     # 打印当前块的内容
                     print(choice.delta.content)
-                    content = choice.delta.content.replace("<think>", "```\n")
-                    content = content.replace("</think>", "\n```\n")
+                    content = choice.delta.content.replace("<think>", "思考中... \n ")
+                    content = content.replace("</think>", "\n思考结束\n ***")
                     yield content
 
 
